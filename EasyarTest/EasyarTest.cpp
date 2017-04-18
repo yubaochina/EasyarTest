5866681cdd30f2d407f19f25110165cbb0929e5a
yubaochina <yubaoliu89@gmail.com>
2017-04-18 18:16:18 +0800

    add dynamic marker using desktop texture

--- EasyarTest/EasyarTest.cpp
+++ EasyarTest/EasyarTest.cpp
@@ -1,6 +1,6 @@
 // EasyAR.cpp : Defines the entry point for the console application.
 //
-
+#include "windows.h"
 #include "stdafx.h"
 #include <windows.h>
 #include <iostream>
@@ -21,6 +21,7 @@
 using namespace cv;
 using namespace std;
 using namespace EasyAR;
+cv::Mat hwnd2mat();
 int main()
 {
 	AugmentedTarget augmentTarget;
@@ -34,8 +35,8 @@ int main()
 	}
 	//configure camera
 	
-	if (!cameraDevice.open(CameraDevice::Device::kDeviceDefault))
-	///if (!cameraDevice.open(3))
+	//if (!cameraDevice.open(CameraDevice::Device::kDeviceDefault))
+	if (!cameraDevice.open(2))
 	{
 		cout << "Open camera failed!" << endl;
 	}
@@ -73,12 +74,14 @@ int main()
 	imshow("preview", showimg);
 #endif
 	//add target image
+	cv::Mat desktopImage = hwnd2mat();
+	cv::imwrite("desktop.jpg", desktopImage);
 	std::string jstr="{\n"
 			"	\"images\"	:\n"
 			"	[\n"
 			"		{\n"
-			"			\"image\"	:	\"lego.jpg\",\n"
-			"			\"name\" : \"lego\"\n"
+			"			\"image\"	:	\"desktop.jpg\",\n"
+			"			\"name\" : \"desktop\"\n"
 			"    }\n"
 			"  ]\n"
 			"}";
@@ -225,3 +228,54 @@ int main()
 	return 0;
 }
 
+cv::Mat hwnd2mat()
+{
+
+	HDC hwindowDC, hwindowCompatibleDC;
+
+	int height, width, srcheight, srcwidth;
+	HBITMAP hbwindow;
+	cv::Mat src;
+	BITMAPINFOHEADER  bi;
+
+	hwindowDC = GetDC(GetDesktopWindow());
+	hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
+	SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
+
+	RECT windowsize;    // get the height and width of the screen
+	GetClientRect(GetDesktopWindow(), &windowsize);
+
+	srcheight = 1080;
+	srcwidth = 1920;
+	height = windowsize.bottom / 4;  //change this to whatever size you want to resize to
+	width = windowsize.right / 4;
+
+	src.create(height, width, CV_8UC4);
+
+	// create a bitmap
+	hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
+	bi.biSize = sizeof(BITMAPINFOHEADER);    //http://msdn.microsoft.com/en-us/library/windows/window/dd183402%28v=vs.85%29.aspx
+	bi.biWidth = width;
+	bi.biHeight = -height;  //this is the line that makes it draw upside down or not
+	bi.biPlanes = 1;
+	bi.biBitCount = 32;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0;
+	bi.biXPelsPerMeter = 0;
+	bi.biYPelsPerMeter = 0;
+	bi.biClrUsed = 0;
+	bi.biClrImportant = 0;
+
+	// use the previously created device context with the bitmap
+	SelectObject(hwindowCompatibleDC, hbwindow);
+	// copy from the window device context to the bitmap device context
+	StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, 0, 0, srcwidth, srcheight, SRCCOPY); //change SRCCOPY to NOTSRCCOPY for wacky colors !
+	GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, src.data, (BITMAPINFO *)&bi, DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
+
+																									   // avoid memory leak
+	DeleteObject(hbwindow);
+	DeleteDC(hwindowCompatibleDC);
+	ReleaseDC(GetDesktopWindow(), hwindowDC);
+
+	return src;
+}
